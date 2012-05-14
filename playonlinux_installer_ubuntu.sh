5e67f886bca8b344d07f492cd33d16bb84e4f52c
Fernando A. Damião <me@fadamiao.com>
2012-05-14 09:06:11 -0300

    Refactoring in playonlinux_installer_ubuntu.sh

--- playonlinux_installer_ubuntu.sh
+++ playonlinux_installer_ubuntu.sh
@@ -1,35 +1,14 @@
 #!/bin/bash
 # ====== Ubuntu PlayOnLinux Installer ======
-UBUNTUVERSION = $(lsb_release -c | awk '{ print $2 }')
+UBUNTUVERSION=$(lsb_release -c | awk '{ print $2 }')
 
 echo "--> Welcome to PlayOnLinux Ubuntu Installer <--"
-sleep 5
+sleep 3
 
 case $UBUNTUVERSION in
-	oneiric)
+	oneiric|natty|maverick|lucid)
 		wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
-		sudo wget http://deb.playonlinux.com/playonlinux_oneiric.list -O /etc/apt/sources.list.d/playonlinux.list
-		sudo apt-get update
-		sudo apt-get install playonlinux
-	;;
-
-	natty)
-		wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
-		sudo wget http://deb.playonlinux.com/playonlinux_natty.list -O /etc/apt/sources.list.d/playonlinux.list
-		sudo apt-get update
-		sudo apt-get install playonlinux
-	;;
-
-	maverick)
-		wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
-		sudo wget http://deb.playonlinux.com/playonlinux_maverick.list -O /etc/apt/sources.list.d/playonlinux.list
-		sudo apt-get update
-		sudo apt-get install playonlinux
-	;;
-
-	lucid)
-		wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
-		sudo wget http://deb.playonlinux.com/playonlinux_lucid.list -O /etc/apt/sources.list.d/playonlinux.list
+		sudo wget "http://deb.playonlinux.com/playonlinux_$UBUNTUVERSION.list" -O /etc/apt/sources.list.d/playonlinux.list
 		sudo apt-get update
 		sudo apt-get install playonlinux
 	;;
