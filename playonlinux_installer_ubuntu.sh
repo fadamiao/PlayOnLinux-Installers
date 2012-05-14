e4a9c0bd49faaf6d188347c3d43c554161c086bd
Fernando A. Damião <me@fadamiao.com>
2012-05-14 13:22:37 -0300

    Update in playonlinux_installer_ubuntu.sh - Precise Pangolin support added

--- playonlinux_installer_ubuntu.sh
+++ playonlinux_installer_ubuntu.sh
@@ -3,10 +3,10 @@
 UBUNTUVERSION=$(lsb_release -c | awk '{ print $2 }')
 
 echo "--> Welcome to PlayOnLinux Ubuntu Installer <--"
-sleep 3
+sleep 2
 
 case $UBUNTUVERSION in
-	oneiric|natty|maverick|lucid)
+	precise|oneiric|natty|maverick|lucid)
 		wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
 		sudo wget "http://deb.playonlinux.com/playonlinux_$UBUNTUVERSION.list" -O /etc/apt/sources.list.d/playonlinux.list
 		sudo apt-get update
