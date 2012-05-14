be91812573c861ce334f2cc3050de42a5ce59e55
Fernando A. Damião <me@fadamiao.com>
2012-05-14 08:55:31 -0300

    Refactoring in playonlinux_installer_fedora.sh

--- playonlinux_installer_fedora.sh
+++ playonlinux_installer_fedora.sh
@@ -4,37 +4,34 @@ POLREPDIR='/etc/yum.repos.d'
 POLREPFILE="$POLREPDIR/playonlinux.repo"
 POLREPURL='http://rpm.playonlinux.com/playonlinux.repo'
 
-echo "--> Welcome to PlayOnLinux Fedora Installer <--"
-sleep 5
+echo '--> Welcome to PlayOnLinux Fedora Installer <--'
+sleep 3
 
 if [ -d $POLREPDIR ];then
+
+	echo '--> Grabbing the repo file on PlayOnLinux Server'
+	sleep 3
+	wget "$POLREPURL" -O "$POLREPFILE"
+
 	if [ $USER = root ]; then
-		echo "--> Grabbing the repo file on PlayOnLinux Server"
-		sleep 5
-		wget "$POLREPURL" -O "$POLREPFILE"
-		
 		if [ -f $POLREPFILE ]; then
-			echo "--> Installing PlayOnLinux"
-			sleep 5
+			echo '--> Installing PlayOnLinux'
+			sleep 3
 			yum update
 			yum install playonlinux
 		else
-			echo "--> The installer can not find the file '$POLREPFILE', please run the installer again"
+			echo '--> The installer can not find the file '$POLREPFILE', please run the installer again'
 		fi
 	else
-		echo "--> Grabbing the repo file on PlayOnLinux Server"
-		sleep 5
-		sudo wget "$POLREPURL" -O "$POLREPFILE"
-		
 		if [ -f $POLREPFILE ]; then
-			echo "--> Installing PlayOnLinux"
-			sleep 5
+			echo '--> Installing PlayOnLinux'
+			sleep 3
 			sudo yum update
 			sudo yum install playonlinux
 		else
-			echo "--> The installer can not find the file '$POLREPFILE', please run the installer again"
+			echo '--> The installer can not find the file '$POLREPFILE', please run the installer again'
 		fi
 	fi
 else
-	echo "--> This is really the Fedora Linux? Please visit the PlayOnLinux website http://playonlinux.com"
+	echo '--> This is really the Fedora Linux? Please visit the PlayOnLinux website http://playonlinux.com'
 fi
